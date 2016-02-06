b3e66e532abd2999357e985d982551984d9330df
Jon Tippens <jwtippens@gmail.com>
2016-02-06 13:05:51 -0800

    Rendered d3Globe in react, no names

--- src/app.js
+++ src/app.js
@@ -1,43 +1,49 @@
-// import {d3Globe} from './components/d3.js';
+import d3Globe from './components/d3.js';
 import React from 'react';
 import d3 from 'd3';
-import approach1 from './components/approach1.js';
-import approach2 from './components/approach2.js';
+// import approach1 from './components/approach1.js';
+// import approach2 from './components/approach2.js';
+// import world from './world-110m.json';
+// import names from './world-country-names.tsv';
 
 // document.write("Hello, DOM");
 
-// var Globe = React.createClass({
-//   // componentDidMount: function () {
-//   //   React.getDOMNode(this).appendChild();
-//   // },
-//   render: function() {
-//     return <h1>"React is rendering!"</h1>;
+var Globe = React.createClass({
+  componentDidMount: function() {
+    d3Globe.go();
+  },
+  render: function () {
+    return (
+      <div className="Globe">
+      </div>
+    );
+  }
+})
+
+// var Canvas = React.createClass({
+//   componentDidMount: function() {
+//     approach1.graph();
+//   },
+//   render: function () {
+//     return (
+//       <div className="approach1">
+//       </div>
+//     );
 //   }
-// })
+// });
 
-var Canvas = React.createClass({
-  componentDidMount: function() {
-    approach1.graph();
-  },
-  render: function () {
-    return (
-      <div className="approach1">
-      </div>
-    );
-  }
-});
+// var Canvas2 = React.createClass({
+//   componentDidMount: function() {
+//     approach2.setup();
+//   },
+//   render: function () {
+//     return (
+//       <div className="approach2">
+//       </div>
+//     );
+//   }
+// });
 
-var Canvas2 = React.createClass({
-  componentDidMount: function() {
-    approach2.setup();
-  },
-  render: function () {
-    return (
-      <div className="approach2">
-      </div>
-    );
-  }
-});
-
+React.render(<Globe />, document.body);
 // React.render(<Canvas />, document.body);
-React.render(<Canvas2 />, document.body);
+// React.render(<Canvas2 />, document.body);
