095fd5c092dab749b3639d9c00f902cf4206fd3a
Juan <jguardado85@gmail.com>
2016-02-05 19:56:07 -0800

    there is connection with db

--- server/sequelizeDB.js
+++ server/sequelizeDB.js
@@ -1,4 +1,7 @@
 var Sequelize = require('sequelize');
+var mysql = require('mysql');
+
+//TODO: Am I adding the instantion of the database in the wrong file?
 
 var sequelize = new Sequelize('worldMapDB', 'root', '', {
   host: 'localhost',
@@ -12,16 +15,95 @@ var sequelize = new Sequelize('worldMapDB', 'root', '', {
 
 });
 
-var User = sequelize.define('User', {
-  username: Sequelize.STRING,
-});
+//TODO: Review. This is an example I found online. I will be removing the password component and replacing it with the capital name
+
+var Country = sequelize.define('country', {
+    countryName: Sequelize.STRING,
+  },
+
+  {
+    //these are just like the getter and setter method in th sequelize docs
+    instanceMethods: {
+      //TODO: read
+      retrieveAll: function(onSuccess, onError) {
+        console.log('feeling lucky');
+
+        //TODO: make sure this is working correctly
+        Country.findAll({});
+
+      },
+
+      // retrieveById: function(user_id, onSuccess, onError) {
+      //   Country.findOne({ where: { id: user_id } }, { raw: true })
+      // .success(onSuccess).error(onError);
+      // },
+
+      //TODO: create
+      add: function(name) {
+        var countryName = name;
+
+        Country.build({ countryName: countryName })
+        .save();
+      },
+
+      //TODO: update
+      updateById: function(user_id, onSuccess, onError) {
+        var id = user_id;
+        var countryName = this.username;
+
+        Country.update({ country: username }, { where: { id: id } });
+
+      },
+
+      //TODO: delete
+      removeById: function(user_id, onSuccess, onError) {
+        Country.destroy({ where: { id: user_id } });
+      },
+    },
+  });
 
 sequelize.sync().then(function() {
-  return User.create({
-    username: 'Tommy Boy',
-  });
-}).then(function(tommy) {
-  console.log(tommy.get({
-    plain: true,
-  }));
+  console.log('this is synced');
 });
+
+Country.build().add('england');
+
+// Country.build().retrieveAll();
+module.exports = { Country: Country };
+
+//Make it crud
+
+// var User = sequelize.define('User', {
+//   username: Sequelize.STRING,
+//   location: Sequelize.STRING,
+// });
+
+//TODO: find out why these promises do not work
+// sequelize.sync().then(function() {
+//   return User.create({
+//     username: 'Tommy Boy',
+//     location: '',
+//   });
+// }).then(function(tommy) {
+//   tommy.update({ location: 'Compton, CA' });
+// });
+
+// .then(function(tommy) {
+//   tommy.update({
+//     location: 'Compton, CA',
+//   });
+// });
+
+// User.findAll().then(function(countries) {
+//   console.log(countries.length);
+// });
+
+// User.update({
+//   username: 'Tommy Gun',
+// }).then(function(tommy) {
+//   console.log(tommy.get({
+//
+//   }))
+// })
+
+// })
--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ var webpack = require('webpack');
 var path = require('path');
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('../webpack.config.js');
+var bodyParser = require('body-parser');
+var db = require('./sequelizeDB.js');
+var mysql = require('mysql');
 
 var app = express();
-var db = require('./sequelizeDB.js');
+app.use(bodyParser());
+
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/../'));
@@ -18,13 +22,113 @@ new WebpackDevServer(webpack(config), {
   hot: true,
   historyApiFallback: true,
   proxy: {
-    "*": "http://localhost:3000"
-  }
-}).listen(3001, 'localhost', function (err, result) {
+    '*': 'http://localhost:3000',
+  },
+}).listen(3001, 'localhost', function(err, result) {
   if (err) {
     console.log(err);
   }
-   console.log('Listening at localhost:3001');
+
+  console.log('Listening at localhost:3001');
 });
 
-console.log('karun says server works');
+// console.log(db.Country.build().retrieveAll());
+
+//this is copied from online also... I want to weed through it and find what is pertinent
+//it uses the instance methods created in the Sequlize file to issue the GET and POST requests
+//
+// =============================================================================
+//TODO: find out what exppress.router is.
+// var router = express.Router();
+
+// on routes that end in /users
+// ----------------------------------------------------
+//this sets all of the below PUT, GET, etc routes to 'users'
+// router.route('/users')
+
+// create a user (accessed at POST http://localhost:3000/api/users)
+app.post('/users', function(req, res) {
+  //console.log('inside post request', req);
+  var username = req.body.username; //bodyParser does the magic
+  // var password = req.body.password;
+
+  var country = db.Country.build({ country: username });
+
+  country.add(function(success) {
+    res.json({ message: 'Country added!' });
+  },
+
+	function(err) {
+  res.send(err);
+	});
+});
+
+// get all the users (accessed at GET http://localhost:8080/api/users)
+app.get('/users', function(req, res) {
+  var country = db.Country.build();
+
+  country.retrieveAll(function(users) {
+    if (users) {
+      res.json(users);
+    } else {
+      res.send(401, 'Country not found');
+    }
+  }, function(error) {
+
+    res.send('Country not found');
+  });
+});
+
+// update a user (accessed at PUT http://localhost:8080/api/users/:user_id)
+app.put(function(req, res) {
+  //This is using the same instance methods from our sequlize page
+  var country = db.Country.build();
+
+  country.username = req.body.username;
+
+  // user.password = req.body.password;
+
+  country.updateById(req.params.user_id, function(success) {
+    //console.log(success);
+    if (success) {
+      res.json({ message: 'User updated!' });
+    } else {
+      res.send(401, 'User not found');
+    }
+  }, function(error) {
+
+    res.send('User not found');
+  });
+});
+
+// get a user by id(accessed at GET http://localhost:8080/api/users/:user_id)
+app.get(function(req, res) {
+  var country = db.Country.build();
+
+  country.retrieveById(req.params.user_id, function(users) {
+    if (users) {
+      res.json(users);
+    } else {
+      res.send(401, 'User not found');
+    }
+  }, function(error) {
+
+    res.send('User not found');
+  });
+});
+
+// delete a user by id (accessed at DELETE http://localhost:8080/api/users/:user_id)
+// app.delete(function(req, res) {
+//   var country = Country.build();
+//
+//   country.removeById(req.params.user_id, function(users) {
+//     if (users) {
+//       res.json({ message: 'User removed!' });
+//     } else {
+//       res.send(401, 'User not found');
+//     }
+//   }, function(error) {
+//
+//     res.send('User not found');
+//   });
+// });
