b4d97e696f07232fe30d741e8d1ced41c266e33b
Jon Tippens <jwtippens@gmail.com>
2016-02-05 20:05:57 -0800

    Appended a graph to the DOM using react and d3

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 // import {d3Globe} from './components/d3.js';
 import React from 'react';
+import d3 from 'd3';
+import approach1 from './components/approach1.js';
 
 // document.write("Hello, DOM");
 
-var Globe = React.createClass({
-  // componentDidMount: function () {
-  //   React.getDOMNode(this).appendChild();
-  // },
-  render: function() {
-    return <h1>"React is rendering!"</h1>;
+// var Globe = React.createClass({
+//   // componentDidMount: function () {
+//   //   React.getDOMNode(this).appendChild();
+//   // },
+//   render: function() {
+//     return <h1>"React is rendering!"</h1>;
+//   }
+// })
+
+// React.render(<Globe />, document.body);
+
+// var Globe = React.createClass({
+//   render: function () {
+//     return (
+//       <div className="Globe">
+//       </div>
+//     );
+//   }
+// });
+// React.render(<Globe />, document.body);
+
+var Canvas = React.createClass({
+  componentDidMount: function() {
+    approach1.graph();
+  },
+  render: function () {
+    return (
+      <div className="approach1">
+      </div>
+    );
   }
-})
-
-React.render(<Globe />, document.body);
-
+});
+React.render(<Canvas />, document.body);
