ebe9e2a3d0d3d0d95a6d91b071a0db18443a06e9
Juan <jguardado85@gmail.com>
2016-02-12 14:47:36 -0800

    starting on context boxes

--- src/components/app.js
+++ src/components/app.js
@@ -4,6 +4,7 @@ import CountryList from '../containers/countryList';
 import TwitterFeed from './twitter_feed';
 import Globe from '../containers/globeContainer';
 import ActiveCountry from '../containers/country-view';
+import Intro from './introduction';
 
 //Globe
 
@@ -11,6 +12,7 @@ export default class App extends Component{
   render() {
     return (
       <div className="main">
+        <Intro />
         <CountryList />
         <div className="col-md-8 globe">
           <Globe />
---
+++ src/components/introduction.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Intro extends Component {
+ render() {
+   return (
+     <div>Intro markdown should go here</div>
+   )
+ }
+}
